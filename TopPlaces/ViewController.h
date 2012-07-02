a508381c901a9876ec215dc7a57940672426b850
Timnit Gebru <tgebru@gmail.com>
2012-07-02 17:40:03 -0400

    Initial Commit

---
+++ TopPlaces/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TopPlaces
+//
+//  Created by timnit gebru on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
